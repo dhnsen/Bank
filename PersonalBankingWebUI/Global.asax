ce817d36d2a74309c3ff891a3bd895cff29a5150
Dan <spag1024@hotmail.com>
2018-10-10 05:57:20 -0500

    Found .NET Core was not compatible with my dependency injection and unit testing tools. Deprecated entire project. Created .NET Standard project, rebuilt class libraries and started repopulating classes

---
+++ PersonalBankingWebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PersonalBankingWebUI.MvcApplication" Language="C#" %>
---
+++ PersonalBankingWebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PersonalBankingWebUI.Startup))]
+namespace PersonalBankingWebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
