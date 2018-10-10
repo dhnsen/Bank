1894c914f3ad9ffd89cacc0a5394746b88370ac4
Dan <spag1024@hotmail.com>
2018-10-10 06:17:18 -0500

    Got project almost back to where it was in dealing with the transaction object. I need to finish the mock repository. Then I can move on to the WebUI

---
+++ DataObjects/Abstract/TransactionAbstract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataObjects.Abstract
+{
+    public abstract class TransactionAbstract
+    {
+        String Id { get; set; }
+        Decimal Amount { get; set; }
+        String Date { get; set; }
+        String Category { get; set; }
+        String Description { get; set; }
+        String Status { get; set; }
+    }
+}
